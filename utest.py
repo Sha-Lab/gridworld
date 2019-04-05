f8fd4c47245fe96cedc52f7ce2e3ad6ed095bc13
Hexiang Hu <frank.hexiang@gmail.com>
2019-04-04 21:00:01 -0700

    remove redundant comments from utest.py

--- utest.py
+++ utest.py
@@ -22,9 +22,6 @@ def control(env, save_gif=False):
     done = False
     discount = 0.99
     imgs = [] # test generate gif
-    #rs = [] # for testing optimal action
-    #estimate_v = env.unwrapped.test_v(discount)
-    #estimate_v = env.get_v(discount)
     while not done:
         env.render()
         print(env.unwrapped.get_q(discount))
@@ -40,10 +37,8 @@ def control(env, save_gif=False):
             env.reset((int(map_id), int(task_id)))
         elif c == 'o':
             _, r, done, _ = env.step(env.get_opt_action())
-            #rs.append(r)
         elif c == 'O':
             _, r, done, _ = env.step(env.get_random_opt_action(discount))
-            #rs.append(r)
         elif c not in CONTROL_MAP:
             continue
         else:
@@ -57,19 +52,13 @@ def control(env, save_gif=False):
                 print('next goal:', env.unwrapped.object_pos[env.unwrapped.task[env.unwrapped.task_idx]]) # for debugging
             diff = abs(r + discount * env.get_v(discount) - qs[a])
             assert diff < 1e-7, 'qs is not consistent! diff: {}'.format(diff)
-    #assert np.isclose(discount_cumsum(rs)[0], estimate_v)
     if save_gif:
         imageio.mimsave('test.gif', imgs, 'GIF', duration=0.2)
 
 def record_trajs(env, filename, index=None):
     assert HAS_DISPLAY
     from py_tools.common import fsave, Recorder, Storage
-    #map_names = ['map{}'.format(i) for i in range(11, 21)]
-    #train_combos = list(product(range(10), range(10)))#[(0, 0), (0, 1), (0, 5)]
-    #test_combos = train_combos
     storage = Storage('env')
-    #env = PORGBEnv(PickGridWorld(map_names, num_obj_types=5, train_combos=train_combos, test_combos=test_combos, window=4, seed=0))
-    #env = get_pick_env(l=3)
     env = Recorder(env, call_media=lambda x: storage.push(x.name, dict(input=x.input, output=x.output)), attr_filter=lambda attr: attr in ['step', 'pretty_render', 'get_opt_action', 'get_q'])
     for _ in range(3):
         done = False
@@ -92,12 +81,7 @@ def record_trajs(env, filename, index=None):
 
 def test_trajs(env, filename, index=None):
     from py_tools.common import fload, Storage, Recorder
-    #map_names = ['map{}'.format(i) for i in range(11, 21)]
-    #train_combos = list(product(range(10), range(10)))#[(0, 0), (0, 1), (0, 5)]
-    #test_combos = train_combos
     storage = Storage('env')
-    #env = PORGBEnv(PickGridWorld(map_names, num_obj_types=5, train_combos=train_combos, test_combos=test_combos, window=4, seed=0))
-    #env = get_pick_env(l=3)
     env = Recorder(env, call_media=lambda x: storage.push(x.name, dict(input=x.input, output=x.output)), attr_filter=lambda attr: attr in ['step', 'pretty_render', 'get_opt_action', 'get_q'])
     env.reset(index=index, sample_pos=True, train=True)
     if HAS_DISPLAY: env.render()
@@ -119,24 +103,19 @@ def test_trajs(env, filename, index=None):
     records = storage.get_lists()
     np.testing.assert_equal(origin_records['pretty_render'], records['pretty_render'])
     if isinstance(env.unwrapped, PickGridWorld):
-        #np.testing.assert_equal(origin_records['get_qs'], records['get_q']) # old records
         np.testing.assert_equal(origin_records['get_q'], records['get_q'])
-        #np.testing.assert_equal([record['output'][0][0] for record in origin_records['step']], [record['output'][0] for record in records['step']]) # old
         np.testing.assert_equal([record['output'][0] for record in origin_records['step']], [record['output'][0] for record in records['step']])
     np.testing.assert_equal(origin_records['get_opt_action'], records['get_opt_action'])
     np.testing.assert_equal([record['output'][1] for record in origin_records['step']], [record['output'][1] for record in records['step']])
     np.testing.assert_equal([record['output'][2] for record in origin_records['step']], [record['output'][2] for record in records['step']])
-    #np.testing.assert_equal(origin_records['step'], records['step']) # this!
 
 def test_planner(env, discount):
     from tqdm import tqdm
     for i in tqdm(range(200)):
         env.reset(sample_pos=True)
-        #env.render()
         done = False
         traj_len = 0
         while not done:
-            #time.sleep(0.01)
             a = env.get_opt_action()
             qs = env.get_q(discount)
             assert a in np.nonzero(np.max(qs) == qs)[0], 'not optimal!'
@@ -145,7 +124,6 @@ def test_planner(env, discount):
             _, r, done, _ = env.step(a)
             traj_len += 1
             assert r > -0.02, 'hit the wall somehow!' # 0.02 is bad...
-            #env.render()
         assert r >= env.success_reward, 'does not success!'
         assert traj_len >= env.unwrapped.min_dis, 'minimum length condition does not hold!'
 
@@ -188,7 +166,6 @@ class TestPickEnv(unittest.TestCase):
 
     @ipdb_on_exception
     def control(self):
-        #env = get_pick_env(task_length=1, num_obj_types=26)
         env = PORGBEnv(
             PickGridWorld(
                 map_names=['map0'],
